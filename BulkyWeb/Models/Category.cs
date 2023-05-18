bb8e7d398ce87ed799df086853a584f801fa44fa
alper <basdoganalper@gmail.com>
2023-05-18 23:52:03 +0300

    changes to connection string

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }             //if the name is Id the framework automatically sees this as primary key
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
